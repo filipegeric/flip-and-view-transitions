c9458acfcf6a045b3f76ea645b96e9dcda6a338c
Filip Egeric <filip.egeric@gmail.com>
2023-09-18 18:42:12 +0200

    Add box

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,7 @@
-import './style.css'
+import './style.css';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-    <h1>FLIP animations and View Transitions API</h1>
+    <div class="box"></div>
   </div>
-`
-
+`;
