88334b4605088bf24eca1acece3db82399c90d7a
Filip Egeric <filip.egeric@gmail.com>
2023-09-21 09:55:07 +0200

    Add toggle for selecting animation type

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,40 @@
+import { moveWithFlip } from './moveWithFlip';
 import { moveWithViewTransition } from './moveWithViewTransition';
 import './style.css';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
+    <label id="toggle-label" onclick="e => e.stopPropagation()">
+      Use View Transitions API
+      <input type="checkbox" />
+    </label>
     <div class="box"></div>
   </div>
 `;
 
-document.addEventListener('click', moveBox);
+document.addEventListener('click', (e) => {
+  const target = e.target as HTMLElement;
+  if (
+    target.id === 'toggle-label' ||
+    target.parentElement!.id === 'toggle-label'
+  ) {
+    e.stopPropagation();
+    return;
+  }
+  moveBox();
+});
 
 function moveBox() {
   const box = document.querySelector('.box') as HTMLDivElement;
+  const useViewTransitionsApi = (
+    document.querySelector('input')! as HTMLInputElement
+  ).checked;
 
-  moveWithViewTransition(box, () => toggleClasses(box));
+  if (useViewTransitionsApi) {
+    moveWithViewTransition(box, () => toggleClasses(box));
+  } else {
+    moveWithFlip(box, () => toggleClasses(box));
+  }
 }
 
 function toggleClasses(box: HTMLDivElement) {
