44e84b162f2f02f51a90ca42380f34adb7e01636
Filip Egeric <filip.egeric@gmail.com>
2023-09-21 09:04:34 +0200

    Animate box move with basic FLIP

--- src/main.ts
+++ src/main.ts
@@ -11,5 +11,19 @@ document.addEventListener('click', moveBox);
 function moveBox() {
   const box = document.querySelector('.box')!;
 
+  const first = box.getBoundingClientRect();
+
   box.classList.toggle('float-right');
+
+  const last = box.getBoundingClientRect();
+
+  const invertX = first.x - last.x;
+
+  box.animate(
+    [{ transform: `translateX(${invertX}px)` }, { transform: 'none' }],
+    {
+      duration: 300,
+      easing: 'ease-in-out',
+    }
+  );
 }
